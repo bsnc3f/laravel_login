ed2a8a35d0e3d1504d6bcf35dad527ba8bd37c9d
bsnc3f <bsnc3f@gmial.com>
2021-04-10 18:05:33 +0900

    ログインフォーム作成、バリデーション設定

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
+use Laravel\Ui\AuthCommand;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [AuthController::class, 'showLogin'])->name('showLogin');
+
+Route::post('login', [AuthController::class, 'login'])->name('login');
