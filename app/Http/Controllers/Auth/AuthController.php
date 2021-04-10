a23c634676f28f8740e57c9b85e6bb1ab0ae1a99
bsnc3f <bsnc3f@gmial.com>
2021-04-10 23:46:29 +0900

    create login/logout

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginFormRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -21,7 +22,36 @@ class AuthController extends Controller
      */
     public function login(LoginFormRequest $request)
     {
-        dd($request->all());
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            // return redirect()->route('home')->with('login_success', 'ログインに成功しました');
+            return redirect()->route('home')->with('success', 'ログインに成功しました');
+        }
+        // return back()->withErrors([
+        //     'login_error' => 'メールアドレスかパスワードが間違っています',
+        return back()->withErrors([
+            'danger' => 'メールアドレスかパスワードが間違っています',
+        ]);
     }
-    //
+
+    /**
+     * ユーザーをアプリケーションからログアウトさせる
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        // return redirect()->route('login.show')->with('logout', 'ログアウトしました');
+        return redirect()->route('login.show')->with('danger', 'ログアウトしました');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,19 @@ use Laravel\Ui\AuthCommand;
 //     return view('welcome');
 // });
 
-Route::get('/', [AuthController::class, 'showLogin'])->name('showLogin');
+Route::middleware(['guest'])->group(function () {
+    //
+    // ログインフォーム表示
+    Route::get('/', [AuthController::class, 'showLogin'])->name('login.show');
+    // ログイン処理
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+});
 
-Route::post('login', [AuthController::class, 'login'])->name('login');
+Route::middleware(['auth'])->group(function () {
+    // ホーム画面
+    Route::get('home', function () {
+        return view('home');
+    })->name('home');
+    // ログアウト
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+});
